ec982649c956f23deff6117bed3d81d6edb755c7
glee1 <gene@Genes-MacBook-Pro.local>
2017-02-17 18:56:16 -0800

    More notes on settings from lecture. HW6 potentially complete

---
+++ Notes/SettingsTest/SettingsTest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  SettingsTest
+//
+//  Created by Gene Lee on 2/16/17.
+//  Copyright © 2017 Gene Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var myTextLabel: UILabel!
+    
+    func updateText () {
+        let myText = UserDefaults.standard.string(forKey: "name_preference")!
+        myTextLabel.text = "Text Field = \(myText)"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        updateText()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        updateText()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
